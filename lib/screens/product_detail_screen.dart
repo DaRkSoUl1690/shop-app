e8eb50b978ca1087a0c2b29f5e6af92fdaaa237b
Vedant Tiwari <tiwarivedant.1690@gmail.com>
2021-12-27 13:44:57 +0530

    designed Cart page

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -3,24 +3,57 @@ import 'package:provider/provider.dart';
 import 'package:shop_app/provider/products_provider.dart';
 
 class ProductDetailScreen extends StatelessWidget {
-  // final String title;
-
-  // const ProductDetailScreen({Key? key, required this.title}) : super(key: key);
   static const routeName = '/lib/screens/product_detail_screen.dart';
 
   const ProductDetailScreen({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     final productId = ModalRoute.of(context)!.settings.arguments as String;
-    // final loadedProduct = Provider.of<ProductsProvider>(context)
-    //     .items
-    //     .firstWhere((prod) => prod.id == productId);
 
     final loadedProduct = Provider.of<ProductsProvider>(context, listen: false)
         .findById(productId);
     return Scaffold(
       appBar: AppBar(
         title: Text(loadedProduct.title),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              color: Colors.orange[300],
+              width: double.infinity,
+              height: 300,
+              child: Image.network(
+                loadedProduct.imageUrl,
+                fit: BoxFit.fitHeight,
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Text(
+              '\$${loadedProduct.price}',
+              style: const TextStyle(
+                color: Colors.grey,
+                fontSize: 20,
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Container(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 10,
+              ),
+              width: double.infinity,
+              child: Text(
+                loadedProduct.description,
+                textAlign: TextAlign.center,
+                softWrap: true,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
